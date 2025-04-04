2a55b0b9356e043c373c43c8511eeedec5728b82
VM-Sinyor <6262032032dedled@gmail.com>
2025-04-05 00:18:44 +0300

    Added hw-1

--- hw-1.js
+++ hw-1.js
@@ -1,1 +1,74 @@
+// Task 1
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+
+// Task 2
+// const releaseDate = 2007;
+// alert(releaseDate);
+
+// Task 3
+// const nameMaker = ('Брендан Эйх');
+// alert(nameMaker);
+
+// Task 4 
+// let x = 10;
+// let y = 2;
+// let sum = x + y;
+// alert(sum);
+// let difference = x - y;
+// alert(difference);
+// let product = x * y;
+// alert(product);
+// let quotient = x / y;
+// alert(quotient);
+
+// Task 5
+// let result = 2**5;
+// alert(result);
+
+// Task 6
+// let a = 9;
+// let b = 2;
+// let remainder = a % b;
+// alert(remainder);
+
+// Task 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+// Task 8
+// let age = prompt('Сколько вам лет?');
+// alert(age);
+
+// Task 9
+// const user {
+//     name: Vitaliy,
+//     age: 23,
+//     isAdmin: true
+// }
+
+// Task 10
+// const name = prompt('Ваше имя');
+// alert(`Привет, ${name}`);
+
+//Task bonus
+
+// let number = prompt('Загадайде любое число');
+// let numberNew = number * 2;
+// alert(numberNew);
+// let numberSum = numberNew + 10;
+// alert(numberSum);
+// let numberQ = numberSum / 2;
+// alert(numberQ);
+// let numberEnd = numberQ - number;
+// alert(numberEnd);
+
 
