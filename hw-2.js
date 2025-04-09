7152355c358bcd9b5ed6f3f0931e410bd8fd1834
VM-Sinyor <6262032032dedled@gmail.com>
2025-04-10 01:31:35 +0300

    Added hw-2

---
+++ hw-2.js
@@ -0,0 +1,106 @@
+// Task 1
+// let password = 'пароль';
+// let userInput = prompt ('Введите пароль');
+//  if (userInput === password) {
+//     alert('Пароль введён верно');
+//  } else {
+//     alert('Пароль введён не верно');
+//  }
+
+// Task 2
+// let c = 2;
+// console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно');
+
+// c = 0;
+// console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно');
+
+// c = 10;
+// console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно');
+
+// c = -3;
+// console.log(c > 0 && c < 10 ? 'Верно' : 'Неверно');
+
+// Task 3
+// let d = 101;
+// let e = 99;
+// console.log((d > 100 || e > 100) ? "Верно" : "Неверно");
+
+// Task 4
+// let a = '2';
+// let b = '3';
+// // Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+// console.log(a + b);
+// a = Number(a);
+// b = Number(b);
+// console.log(a + b);
+
+// Task 5
+// let monthNumber = 12
+// if (monthNumber < 1 || monthNumber > 12) {
+//     console.log('Такого месяца не существует');
+// } else {
+//     switch (monthNumber) {
+//         case 12:
+//         case 1:
+//         case 2:
+//             console.log('Зима');
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:
+//             console.log('Весна');
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:
+//             console.log('Лето');
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:
+//             console.log('Осень');
+//         break;
+//         default:
+//             break;
+//     }
+// }
+
+// Additional tasks 1
+// let number = Number(prompt('Пожалуйста, введите любое число'));
+// if (!isNaN(number)) {
+//     if (number % 2 === 0) {
+//         alert('Число четное')
+//     } else {
+//         alert('Число нечетное')
+//     }
+// } else {
+//     alert('Вы ввели не число!')
+// }
+
+// Additional tasks 2
+// let clientOS = 0;
+// if (clientOS % 2 === 0) {
+//     console.log('Установите версию приложения для iOS по ссылке');
+// } else {
+//     console.log('Установите версию приложения для Android по ссылке');
+// }
+
+// Additional tasks 3
+// let clientOS = 1;
+// const clientDeviceYear = prompt('Введите год создания телефона');
+
+// if (clientDeviceYear < 2015) {
+//     console.log((clientOS % 2 === 0) ? 'Установите облегченную версию приложения для iOS по ссылке' : 'Установите облегченную версию приложения для Android по ссылке');
+// } else {
+//     console.log((clientOS % 2 === 0) ? 'Установите версию приложения для iOS по ссылке' : 'Установите версию приложения для Android по ссылке');
+// }
+
+
+
+
+
+
+
+
+
+
