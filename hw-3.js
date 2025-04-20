b4fba0c2040c19ede8469ebdb5ddf7480b71c28b
VM-Sinyor <6262032032dedled@gmail.com>
2025-04-21 00:59:50 +0300

    js homework

---
+++ hw-3.js
@@ -0,0 +1,81 @@
+// let number = 1;
+
+// while (number <= 12) {
+//     if (number >= 1 && number <=2 || number === 12) {
+//         console.log('Зима');
+//     } else if (number >= 3 && number <= 5) {
+//         console.log('Весна');
+//     } else if (number >= 6 && number <= 8) {
+//         console.log('Лето');
+//     } else if (number >= 9 && number <= 11) {
+//         console.log('Осень');
+//     }
+
+// 	number++;
+// }
+
+// while (true) {
+//     const pers = prompt("Введите сообщение");
+//     if (pers === 'continue') {
+//         continue;
+//     }
+//     console.log(pers);
+//     if (pers === 'break') {
+//         break;
+//     }
+
+// }
+
+// // Task 1
+// h = 1;
+// do {
+//     h++;
+//     console.log('Привет');
+// } while (h <= 2);
+
+// // Task 2
+// for (let n = 1; n <= 5; n++) {
+//     console.log(n);
+// }
+
+// // Task 3
+// let n = 1;
+// while (n < 22) {
+//     n++;
+//     if (n < 7) {
+//         continue;
+//     }
+//     console.log(n);
+// }
+
+// // Task 4
+// let obj = {
+//     'Коля':'200',
+//     'Вася':'300',
+//     "Петя":'400'
+// }
+
+// for (let key in obj) {
+//     console.log(`${key}: ${obj[key]}$`);
+// }
+
+// Task 5
+// let n = 1000;
+// let num = 0;
+
+// while (n >= 50) {
+//     n /= 2;
+//     num++;
+// }
+//     console.log(n);
+//     console.log(num);
+
+// // Task 6
+// const firstFriday = 6;
+// const month = 31;
+
+// for (let friday = firstFriday; friday <= month; friday += 7) {
+//     console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.'`);
+// }
+
+    
