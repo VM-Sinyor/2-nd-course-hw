3c2be782eed46010098b31de915ebe1915e7ff79
VM-Sinyor <6262032032dedled@gmail.com>
2025-05-27 07:40:08 +0300

    fix

--- hw-7.js
+++ hw-7.js
@@ -1,82 +1,80 @@
 // Task 1
-// const people = [
-//    { name: 'Глеб', age: 29 },
-//    { name: 'Анна', age: 17 },
-//    { name: 'Олег', age: 7 },
-//    { name: 'Оксана', age: 47 }
-// ];
+const peopleByAge = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
 
 
-// console.log(people.sort((a, b) => a.age - b.age));
+console.log(peopleByAge.sort((a, b) => a.age - b.age));
 
 // Task 2
 
-// const isPositive = (num) => {
-//   return num > 0;
-// }
+const isPositive = (num) => {
+  return num > 0;
+}
 
 
-// const isMale = (person) => {
-//   return person.gender === 'male';
-// }
+const isMale = (person) => {
+  return person.gender === 'male';
+}
 
-// const filter = (array, ruleFunction) => {
-//   const result = [];
-//   for (let i = 0; i < array.length; i++) {
-//     if (ruleFunction(array[i])) {
-//       result.push(array[i]);
-//     }
-//   }
-//   return result;
-// }
+const filter = (array, ruleFunction) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (ruleFunction(array[i])) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
 
-// console.log(filter([3, -4, 1, 9], isPositive)); 
+console.log(filter([3, -4, 1, 9], isPositive)); 
 
 
-// const people = [
-//    {name: 'Глеб', gender: 'male'},
-//    {name: 'Анна', gender: 'female'},
-//    {name: 'Олег', gender: 'male'},
-//    {name: 'Оксана', gender: 'female'}
-// ];
+const peopleByGender = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
 
-// console.log(filter(people, isMale));
+console.log(filter(peopleByGender, isMale));
 
 // Task 3
 
-// let elapsed = 0;
-// const interval = 3000; 
+const interval = 3000;
 
-// const timerId = setInterval(() => {
-//   elapsed += 3;
-//   console.log(new Date().toLocaleString()); 
+const timerId = setInterval(() => {
+  console.log(new Date().toLocaleString());
+}, interval);
 
-//   if (elapsed >= 30) {
-//     clearInterval(timerId); 
-//     console.log("30 секунд прошло");
-//   }
-// }, interval);
+setTimeout(() => {
+  clearInterval(timerId);
+  console.log("30 секунд прошло");
+}, 30000);
 
 // Task 4
 
-// const delayForSecond = (callback) => {
-//     setTimeout(callback, 1000); 
-
-// delayForSecond(function () {
-//    console.log('Привет, Глеб!');
-// });
+const delayHello = (callback) => {
+    setTimeout(callback, 1000); 
+};
+delayHello(function () {
+   console.log('Привет, Глеб!');
+});
 
 // Task 5
 
-// function delayForSecond(cb) {
-//     setTimeout(() => {
-//         console.log('Прошла одна секунда');
-//         if(cb) {  cb(); }
-//     }, 1000)
-// }
+function delayThenCallback(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
 
-// function sayHi (name) {
-//     console.log(`Привет, ${name}!`);
-// }
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
 
-// delayForSecond(() => sayHi('Глеб'));
+delayThenCallback(() => sayHi('Глеб'));
